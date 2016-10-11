7d45009db7969c0e7b17a11700b4c90069d7ac11
codergege <codergege@163.com>
2016-10-12 03:52:03 +0800

    install_java done 20161012 03:51

--- install_java.sh
+++ install_java.sh
@@ -2,13 +2,12 @@
 # @file: 
 #	install_java.sh
 # @desc:
+#	如果要安装的 java 版本改变, 脚本中的一些设置也需要改变(java7已经稳定, 一般不会变了): pkg, untar, url
 #	Install java.
-#	jdk-7u80-linux-i586.tar.gz	http://download.oracle.com/otn/java/jdk/7u80-b15/jdk-7u80-linux-i586.tar.gz 
-#	jdk-7u80-linux-x64.tar.gz	http://download.oracle.com/otn/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz
-#	jdk-8u101-linux-i586.tar.gz	http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-i586.tar.gz
-#	jdk-8u101-linux-x64.tar.gz	http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz
+#	7u80的版本链接 otn 改成 otn-pub
 #	1. 判断是否安装了 jdk 
 #		通过 /usr/local/lib/java 文件夹是否存在来判断; 如果存在, echo, exit
+#		通过判断 /etc/profile ~/.bashrc 中是否存在 JAVA_HOME ; 如果存在, echo, exit
 #	2. 判断 $home/develop/installation 下是否有对应的包下载好了, 如果没有, 就 wget 到该目录下
 #	3. 解压缩到 /usr/local/lib/java 下
 #	4. 配置环境变量
@@ -17,17 +16,102 @@
 # @Last Update:
 #	2016-10-11
 # @usage:
-#	bash install_java.sh java7 32
-#	bash install_java.sh java8 64
+#	为了使 souce $config 生效, 要用 source 命令执行
+#	source install_java.sh java7 32
+#	source install_java.sh java8 64
 
-# 只在当前脚本生效, 脚本执行完毕后, 父 shell 中还是原来的 PATH
-PATH=$PATH:~/bin
-export PATH
-# path
+# home
 home=/home/codergege
-profile=/etc/profile
+# 安装包目录
+pkgDir=$home/develop/installation
+# 安装目录
+installDir=/usr/local/lib/java
+# config
+config=/etc/environment
+# 保存当前目录
+pwdDir=$pwd
 
-java_version=$1
+# 检查输入参数
+if [ $1 == "java7" ] || [ $1 == "java8" ]; then
+	echo "安装 $1"
+else
+	echo "必须加入第一个参数 java7 或 java8"
+	exit 1
+fi
+if [ $2 == "32" ] || [ $2 == "64" ]; then
+	echo "安装 $2 位版本"
+else
+	echo "必须加入第二个参数 32 或 64"
+	exit 1
+fi
 
-# 判断 java_version
+# 确定安装包名, 下载网址
+if [ $1 == "java7" ] && [ $2 == "32" ]; then
+	pkg=$pkgDir/jdk-7u80-linux-i586.tar.gz
+	jdk=jdk1.7.0_80
+	url=http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-i586.tar.gz
+fi
+if [ $1 == "java7" ] && [ $2 == "64" ]; then
+	pkg=$pkgDir/jdk-7u80-linux-x64.tar.gz
+	jdk=jdk1.7.0_80
+	url=http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz
+fi
+if [ $1 == "java8" ] && [ $2 == "32" ]; then
+	pkg=$pkgDir/jdk-8u101-linux-i586.tar.gz
+	jdk=jdk1.8.0_101
+	url=http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-i586.tar.gz
+fi
+if [ $1 == "java8" ] && [ $2 == "64" ]; then
+	pkg=$pkgDir/jdk-8u101-linux-x64.tar.gz	
+	jdk=jdk1.8.0_101
+	url=http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz
+fi
+
+# 判断安装包是否存在
+cd $pkgDir
+if [ -f "$pkg" ]; then
+	echo "找到本地安装包, 准备安装..."
+else
+	echo "未找到本地安装包, 准备下载..."
+	wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" -P $pkgDir $url
+fi
+if [ ! -d "$installDir" ]; then
+	sudo mkdir -p $installDir
+fi
+
+# 解压到安装目录
+jdk=$installDir/$jdk
+if [ -d "$jdk" ]; then
+sudo rm -rf $jdk
+fi
+sudo tar -xvf $pkg -C $installDir
+
+# 创建 link 这步不需要
+# cd $installDir
+
+# 将 java 环境变量写入 $config 的函数
+function set_java_env() {
+	sudo sed -i '$a # Set java environment JAVA_HOME' $config
+	echo "JAVA_HOME=$jdk" | sudo tee -a $config
+	sudo sed -i '$a export JAVA_HOME' $config
+	sudo sed -i '$a CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar' $config
+	sudo sed -i '$a export CLASSPATH #JAVA_HOME' $config
+	sudo sed -i '$a PATH=$PATH:$JAVA_HOME/bin' $config
+	sudo sed -i '$a export PATH #JAVA_HOME' $config
+	source $config
+}
+
+# set java environment
+if ! grep "JAVA_HOME" $config; then
+	echo "设置 java environment"
+	set_java_env
+else
+	echo "$env 中已存在 JAVA_HOME, 将删除原设置, 并重新设置"
+	# 如果已经有了设置, 先删除再写入
+	sudo sed -i '/JAVA_HOME/d' $config
+	set_java_env
+fi
+
+cd $pwdDir
+echo "========> $jdk installed"
 
---
+++ uninstall_java.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# @file: 
+#	uninstall_java.sh
+# @desc:
+#	Uninstall java.
+#	delete JAVA_HOME, CLASSPATH, 有 $JAVA_HOME 的 PATH -- 从 /etc/environment, ~/.bashrc, /etc/profile, ~/.profile 中删除
+#	delete /usr/local/lib/java
+#	delete /usr/lib/jvm
+#	delete /etc/alternatives 中所有的 java 链接
+# @Author:
+#	codergege
+# @Last Update:
+#	2016-10-11
+# @usage:
+#	bash uninstall_java.sh
+
+# 只在当前脚本生效, 脚本执行完毕后, 父 shell 中还是原来的 PATH
+PATH=$PATH:~/bin
+export PATH
+# path
+home=/home/codergege
+env=/etc/environment
+bashrc=$home/.bashrc
+profile=/etc/profile
+userProfile=$home/.profile
+
+# 判断 env, bashrc, profile, userProfile 是否存在
+# 如果存在, 则删除 含 JAVA_HOME 的行
+if [ -f "$env" ]; then
+	sudo sed -i '/JAVA_HOME/d' $env
+fi
+if [ -f "$bashrc" ]; then
+	sudo sed -i '/JAVA_HOME/d' $bashrc
+fi
+if [ -f "$profile" ]; then
+	sudo sed -i '/JAVA_HOME/d' $profile
+fi
+if [ -f "$userProfile" ]; then
+	sudo sed -i '/JAVA_HOME/d' $userProfile
+fi
+
+# 删除 /usr/local/lib/java, /usr/lib/jvm
+if [ -d "/usr/local/lib/java" ]; then
+	echo "Deleting /usr/local/lib/java"
+	sudo rm -rf /usr/local/lib/java
+fi
+if [ -d "/usr/lib/jvm" ]; then
+	echo "Deleting /usr/lib/jvm"
+	sudo rm -rf /usr/lib/jvm
+fi
+
+# 删除 /etc/alternatives 中所有的 java 链接
+links=$(ls -al /etc/alternatives | grep 'java' | awk '{print $(NF-2)}')
+cd /etc/alternatives
+for link in $links
+do
+	sudo rm -rf $link
+done
+
+echo "Uninstall java complete"
